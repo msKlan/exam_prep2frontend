0999647493ec8c73b5f2e56a77ff09b040ba99bc
Klan <michaelsanderklan@gmail.com>
2020-01-12 18:09:18 +0100

    Updates

---
+++ precache-manifest.707da192dc64b3e9d7c31e1c7828ec27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7506223df6cc238bff25c5c1126f867c",
+    "url": "/msKlan/exam_prep2frontend/index.html"
+  },
+  {
+    "revision": "2e753efa98bf250b6f3a",
+    "url": "/msKlan/exam_prep2frontend/static/css/main.791c8edd.chunk.css"
+  },
+  {
+    "revision": "7deb1e0f0307243fdf8d",
+    "url": "/msKlan/exam_prep2frontend/static/js/2.581ba2d4.chunk.js"
+  },
+  {
+    "revision": "2e753efa98bf250b6f3a",
+    "url": "/msKlan/exam_prep2frontend/static/js/main.e1ef65e2.chunk.js"
+  },
+  {
+    "revision": "6659cb0c72e2719e81fa",
+    "url": "/msKlan/exam_prep2frontend/static/js/runtime-main.5f0e716d.js"
+  }
+]);
--- src/settings.js
+++
@@ -1,3 +0,0 @@
-// const URL = 'http://localhost:8080/exam_prep2/api';
-const URL = 'https://aieou.dk/exam_prep2/api';
-export default URL;
